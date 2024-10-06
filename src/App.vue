292626ba750920e4612dc0d228f08d3147265a71
ddatunashvili <davitidatunashvili98@gmail.com>
2024-10-06 17:23:15 +0400

    original

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>hello vue</h1>
+    <div class="posts">
+      <Post @sorting="sorting" :info="value" :key="i" v-for="(value, i) in post_sort" />
+    </div>
+  </div>
+</template>
+
+<script>
+import data from "./data.json"
+import Post from "./components/post.vue"
+
+export default {
+  components: {
+    Post
+  },
+  data() {
+    return {
+      posts: []
+    }
+  },
+  created() {
+    this.posts = data
+  },
+  computed: {
+    post_sort() {
+      return this.posts.sort((a, b) => (b.likes || 0) - (a.likes || 0))
+    }
+  },
+  methods: {
+    sorting(post) {
+      // console.log(post)
+      this.posts.sort((a, b) => b.likes - a.likes)
+    }
+  }
+}
+</script>
