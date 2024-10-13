8d3c42ce9dd88264c4480f22b199bc690634fff4
ddatunashvili <davitidatunashvili98@gmail.com>
2024-10-13 17:58:09 +0400

    ref and watchers explained

--- src/App.vue
+++ src/App.vue
@@ -36,6 +36,32 @@
 
     <a @[event]="handle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem corrupti minus iure in incidunt eveniet ab ex quas reiciendis nesciunt sunt modi at ipsam, repudiandae, ea ducimus veniam ipsa expedita? </a>
 
+    <div ref="post">
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque iure, quasi corporis, explicabo dolore commodi ab officia ad quam asperiores dignissimos alias aliquam laudantium, quae rerum quaerat error quos! Autem.</p>
+    </div>
+
+
+    <div>Checked names: {{ checkedNames }}</div>
+
+    <input type="checkbox" id="jack" value="Jack" v-model="checkedNames" />
+    <label for="jack">Jack</label>
+
+    <input type="checkbox" id="john" value="John" v-model="checkedNames" />
+    <label for="john">John</label>
+
+    <input type="checkbox" id="mike" value="Mike" v-model="checkedNames" />
+    <label for="mike">Mike</label>
+
+    <br>
+    <br>
+    <label > {{ text }}</label>
+    
+    <br>
+    <br>
+    <input type="text" v-model.lazy="text" placeholder="text">
+    <br>
+    <br>
+    <input type="text" v-model="formData.name" placeholder="name">
   </div>
 </template>
 
@@ -44,21 +70,50 @@
 export default {
   data(){
     return {
+      checkedNames:[],
+      formData:{
+        name:""
+      },
+
       msg:"gela",
       rawHtml: "<h1>hello</h1>",
       custom_class: "active",
       // data:1,
+      text:"",
       attributeName: "href",
       url:"https://renode.space",
       event: "mouseover" // event name
     }
+  },
+  watch:{
+    text(newValue, old){
+      let nums = "1234567890"
+      for (let i of newValue){
+        if (nums.includes(i)){
+          this.text = ""
+        }
+      }
+      console.log( newValue)
+    },
+    formData: {
+      handler(newVal, old){
+        console.log(newVal)
+      },
+
+
+    }
+  },
+  mounted(){
+    this.$refs.post.addEventListener("click",()=>{
+      this.$refs.post.style.color = "red"
+    })
   },
   methods:{
     doSomething(){
 
     },
     handle(){
-      console.log("hovered")
+      // console.log("hovered")
     }
   }
 }
