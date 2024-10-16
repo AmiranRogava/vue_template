a89598d9c502b14e92f7be7e6347cfd566c47806
ddatunashvili <davitidatunashvili98@gmail.com>
2024-10-16 14:50:07 +0400

    props functio with slot

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div>
-    <child>
-       <p class="active">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aliquam, ipsam odit porro eaque nihil! Quisquam veniam quia veritatis, laudantium nobis officiis qui, fuga quae at voluptate amet ex. Dolorem?</p>
-    </child>
-  </div>
+  <FancyButton :onClick="handleClick">
+    Click me!
+  </FancyButton>
 </template>
 
 <script>
-import child from './components/child.vue';
+import FancyButton from './components/child.vue';
 
 export default {
   components: {
-    child
+    FancyButton
   },
-  data(){
-    return{
-      data:1
+  methods: {
+    handleClick() {
+      alert('Button clicked!');
     }
   }
-
 }
 </script>
-<style scoped>
-.active{
-  color: green;
-}
-</style>
--- src/components/child.vue
+++ src/components/child.vue
@@ -1,12 +1,34 @@
 <template>
- 
-    <div>
-        <slot></slot>
-    </div>
-</template>
-
-<script>
-export default {
-
-}
-</script>
+    <button class="fancy-btn" @click="onClick">
+      <slot></slot>
+    </button>
+  </template>
+  
+  <script>
+  export default {
+    name: 'FancyButton',
+    props: {
+      onClick: {
+        type: Function,
+        required: true
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .fancy-btn {
+    background-color: #4CAF50;
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    cursor: pointer;
+    font-size: 16px;
+    border-radius: 5px;
+  }
+  
+  .fancy-btn:hover {
+    background-color: #45a049;
+  }
+  </style>
+  
