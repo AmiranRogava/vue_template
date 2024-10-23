a63381ae3b36d7903bc69a4a6f831377bb8dbd90
ddatunashvili <davitidatunashvili98@gmail.com>
2024-10-23 16:26:36 +0400

    fetching and dynamic rendering

---
+++ src/components/product.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>{{ prod.title }}</h2>
+    <p>{{ prod.description }}</p>
+    <img :src="prod.image" alt="">
+  
+  </div>
+</template>
+
+<script  >
+export default{
+    props:{
+        prod:Object
+    }
+}
+ 
+</script>
+
+<style>
+
+</style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,19 +2,38 @@
   <div>
         <h1>home</h1>
         <button @click="openInner(name)">open</button>
+    <div class="products">
+      <Prod  :prod="prod" v-for="prod, in products"></Prod> 
+
+    </div>
   </div>
 </template>
 
 <script  >
+import Prod from "../components/product.vue"
+import axios from "axios"
 export default{
+    components:{
+      Prod
+    }
+  ,
   data() {
     return {
-      name:"gela"
+      name:"gela",
+      products: []
     }
+  },
+ created(){
+    this.fetch_prods()
   },
   methods:{
     openInner(name){
       this.$router.push({ name: 'Inner', params: { name } });
+    },
+    async fetch_prods(){
+      const res = await axios.get("https://fakestoreapi.com/products")
+      console.log(res)
+      this.products = res.data
     }
   }
 }
